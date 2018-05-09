7df74bcd25c2aae5628f4a2083f2d5aad09f7db3
anilkunchalaece@gmail.com <anilkunchalaece@gmail.com>
2018-05-09 18:43:14 +0530

    pitch and roll graphs are working

--- PythonCode/GUI/graphGUI.py
+++ PythonCode/GUI/graphGUI.py
@@ -11,20 +11,25 @@ from PyQt4 import QtGui,QtCore
 import pyqtgraph as pg
 import numpy as np
 import serial
+import re
 
-arduinoData = serial.Serial('/dev/ttyACM0', 115200) #Creating our serial object named arduinoData
+arduinoData = serial.Serial('/dev/ttyACM1', 115200) #Creating our serial object named arduinoData
 
-noOfDataSamples = 200
 
-#Create a Empty Array to hold the graph parameters
-pitchSetPoint = np.empty(noOfDataSamples)
-rollSetPoint = np.empty(noOfDataSamples)
+pitchVal = []
+rollVal = []
+yawVal = []
 
-pitchVal = np.empty(noOfDataSamples)
-rollVal = np.empty(noOfDataSamples)
+pitchSetPoint = []
+rollSetPoint = []
+yawSetpoint = []
 
-recvString = ""
 
+
+m0Val = 0
+m1Val = 0
+m2Val = 0
+m3Val = 0
 
 #Always start by initializing Qt
 app = QtGui.QApplication([])
@@ -36,7 +41,9 @@ w = QtGui.QWidget()
 btn = QtGui.QPushButton('Press me')
 text = QtGui.QLineEdit('enter text')
 listw = QtGui.QListWidget()
-plot = pg.PlotWidget()
+pitchPlot = pg.PlotWidget()
+rollPlot = pg.PlotWidget()
+
 
 ##Create a grid layout to manage the widgets size and position
 layout = QtGui.QGridLayout()
@@ -47,29 +54,65 @@ w.setLayout(layout)
 layout.addWidget(btn,0,0)
 layout.addWidget(text,1,0)
 layout.addWidget(listw,2,0)
-layout.addWidget(plot,0,1,3,1)
+layout.addWidget(pitchPlot,0,1)
+layout.addWidget(rollPlot,1,1)
 
 #Display widget as a new window
 w.show()
-
-
-#x = np.random.normal(1000)
-#y = np.random.normal(1000)
-
-def checkData():
-    	noOfBytes = arduinoData.inWaiting()
-    	if(noOfBytes > 0) :
-    			recvString = arduinoData.readline();
-
 
 timer = pg.QtCore.QTimer()
 
 def update():
     	checkData()
-        plot.plot(x,y,clear=True)
+        x1 = np.arange(len(pitchVal))
+        pitchPlot.plot(x1,pitchVal,clear=True,pen=(1,2))
+		#ref : https://stackoverflow.com/questions/48590354/pyqtgraph-plotwidget-multiple-y-axis-plots-in-wrong-area?
+        pitchPlot.addItem(pg.PlotCurveItem(x1,pitchSetPoint,clear=True,pen=(2,2)))
+
+        x2 = np.arange(len(rollVal))
+        rollPlot.plot(x2,rollVal,clear=True,pen=(1,2))
+        rollPlot.addItem(pg.PlotCurveItem(x2,rollSetPoint,clear=True,pen=(2,2)))
 
 timer.timeout.connect(update)
 timer.start(16)
+
+def checkForReadings(data):
+    y = re.findall(r"y([-+]?\d*\.\d+|\d+)",data)
+    p = re.findall(r"p([-+]?\d*\.\d+|\d+)",data)
+    r = re.findall(r"r([-+]?\d*\.\d+|\d+)",data)
+    ps = re.findall(r"ps([-+]?\d*\.\d+|\d+)",data)
+    rs = re.findall(r"rs([-+]?\d*\.\d+|\d+)",data)
+    po = re.findall(r"po([-+]?\d*\.\d+|\d+)",data)
+    ro = re.findall(r"ro([-+]?\d*\.\d+|\d+)",data)
+    m0 = re.findall(r"m0-([-+]?\d*\.\d+|\d+)",data)
+    m1 = re.findall(r"m1-([-+]?\d*\.\d+|\d+)",data)
+    m2 = re.findall(r"m2-([-+]?\d*\.\d+|\d+)",data)
+    m3 = re.findall(r"m3-([-+]?\d*\.\d+|\d+)",data)
+    if len(p) > 0 and len(ps) > 0:
+    	pitchVal.append(float(p[0]))
+        pitchSetPoint.append(float(ps[0]))
+
+    if len(r) > 0 and len(rs) > 0:
+    	rollVal.append(float(r[0]))
+        rollSetPoint.append(float(rs[0]))
+
+    if len(m0) > 0 :
+        m0Val = float(m0[0])
+    if len(m1) > 0 :
+    	m1Val = float(m1[0])
+    if len(m2) > 0 :
+    	m2Val = float(m2[0])
+    if len(m3) > 0 :
+    	m3Val = float(m3[0])
+    # print [y,p,r,ps,rs,po,ro,m0,m1,m2,m3]
+
+def checkData():
+    # print "check data"
+    recvString = '' # to store the data
+    while (arduinoData.inWaiting() > 0):
+        # print arduinoData.read()
+        recvString = recvString + arduinoData.read()
+    checkForReadings(recvString)
 
 
 #start qt event loop
