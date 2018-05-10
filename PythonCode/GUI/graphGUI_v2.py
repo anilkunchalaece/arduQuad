714a72d1760823929ed5c4dda5d49d5bf8d6b069
anilkunchalaece@gmail.com <anilkunchalaece@gmail.com>
2018-05-10 21:33:18 +0530

    GUI V2 is working

--- PythonCode/GUI/graphGUI_v2.py
+++ PythonCode/GUI/graphGUI_v2.py
@@ -62,101 +62,62 @@ w.setWindowTitle("KSRMQuad Monitor By Kunchala Anil")
 layout.addWidget(pitchPlot,0,1)
 layout.addWidget(rollPlot,1,1)
 layout.addWidget(yawPlot,2,1)
-layout.addWidget(motorPlot,2,0)
-layout.addWidget(instantData,0,0,2,1)
+layout.addWidget(instantData,0,0)
+layout.addWidget(motorPlot,1,0)
 
 ## create  a grid layout to display instantaneous data
 instLayout = QtGui.QGridLayout()
 instantData.setLayout(instLayout)
 
 rollInstLable = QtGui.QLabel("Roll")
-rollInstLable.setFrameStyle(QFrame.WinPanel | QFrame.Sunken)
-rollInstVal = QtGui.QLabel()
-rollInstVal.setFrameStyle(QFrame.WinPanel | QFrame.Raised)
-rollInstVal.setText("45")
 pitchInstLable = QtGui.QLabel("Pitch")
-pitchInstLable.setFrameStyle(QFrame.WinPanel | QFrame.Sunken)
-pitchInstVal = QtGui.QLabel()
-pitchInstVal.setFrameStyle(QFrame.WinPanel | QFrame.Sunken)
-pitchInstVal.setText("23")
 yawInstLable = QtGui.QLabel("Yaw")
-yawInstLable.setFrameStyle(QFrame.WinPanel | QFrame.Sunken)
-yawInstVal = QtGui.QLabel()
-yawInstVal.setFrameStyle(QFrame.WinPanel | QFrame.Sunken)
 
-rollPIDOutInstLable = QtGui.QLabel("Roll PID Output")
-rollPIDOutInstLable.setFrameStyle(QFrame.WinPanel | QFrame.Sunken)
-rollPIDOutInstVal = QtGui.QLabel()
-rollPIDOutInstVal.setFrameStyle(QFrame.WinPanel | QFrame.Sunken)
-pitchPIDOutInstLable = QtGui.QLabel("Pitch PID Output")
-pitchPIDOutInstLable.setFrameStyle(QFrame.WinPanel | QFrame.Sunken)
-pitchPIDOutInstVal = QtGui.QLabel()
-pitchInstVal.setFrameStyle(QFrame.WinPanel | QFrame.Sunken)
-yawPIDOutInstLable = QtGui.QLabel("Yaw PID Output")
-yawPIDOutInstLable.setFrameStyle(QFrame.WinPanel | QFrame.Sunken)
-yawPIDOutInstVal = QtGui.QLabel()
-yawPIDOutInstVal.setFrameStyle(QFrame.WinPanel | QFrame.Sunken)
+rollPIDOutInstLable = QtGui.QLabel("Roll PID Out")
+pitchPIDOutInstLable = QtGui.QLabel("Pitch PID Out")
+yawPIDOutInstLable = QtGui.QLabel("Yaw PID Out")
 
 rollSetPointInstLable = QtGui.QLabel("Roll SetPoint")
-rollSetPointInstVal = QtGui.QLabel()
 pitchSetPointInstLable = QtGui.QLabel("Pitch SetPoint")
-pitchSetPointInstVal = QtGui.QLabel()
 yawSetPointInstLable = QtGui.QLabel("Yaw SetPoint")
-yawSetPointInstVal = QtGui.QLabel()
 
 frontMotorInstLable = QtGui.QLabel("Front")
-frontMotorInstVal = QtGui.QLabel()
 rightMotorInstLable = QtGui.QLabel("Right")
-rightMotorInstVal = QtGui.QLabel()
 rearMotorInstLable = QtGui.QLabel("Rear")
-rearMotorInstVal = QtGui.QLabel()
 leftMotorInstLable = QtGui.QLabel("Left")
-leftMotorInstVal = QtGui.QLabel()
 
 ch1InstLable = QtGui.QLabel("CH1-Roll")
-ch1InstVal = QtGui.QLabel()
 ch2InstLable = QtGui.QLabel("CH2-Pitch")
-ch2InstVal = QtGui.QLabel()
 ch3InstLable = QtGui.QLabel("CH3-Throttle")
-ch3InstVal = QtGui.QLabel()
 ch4InstLable = QtGui.QLabel("CH4-Yaw")
-ch4InstVal = QtGui.QLabel()
 
 instLayout.addWidget(rollInstLable,0,0)
-instLayout.addWidget(rollInstVal,0,1)
 instLayout.addWidget(pitchInstLable,1,0)
-instLayout.addWidget(pitchInstVal,1,1)
 instLayout.addWidget(yawInstLable,2,0)
-instLayout.addWidget(yawInstVal,2,1)
 
 instLayout.addWidget(rollPIDOutInstLable,3,0)
-instLayout.addWidget(rollPIDOutInstVal,3,1)
 instLayout.addWidget(pitchPIDOutInstLable,4,0)
-instLayout.addWidget(pitchPIDOutInstVal,4,1)
 instLayout.addWidget(yawPIDOutInstLable,5,0)
-instLayout.addWidget(yawPIDOutInstVal,5,1)
 
-instLayout.addWidget(frontMotorInstLable,6,0)
-instLayout.addWidget(frontMotorInstVal,6,1)
-instLayout.addWidget(rightMotorInstLable,7,0)
-instLayout.addWidget(rightMotorInstVal,7,1)
-instLayout.addWidget(rearMotorInstLable,8,0)
-instLayout.addWidget(rearMotorInstVal,8,1)
-instLayout.addWidget(leftMotorInstLable,9,0)
-instLayout.addWidget(leftMotorInstVal,9,1)
+instLayout.addWidget(rollSetPointInstLable,6,0)
+instLayout.addWidget(pitchSetPointInstLable,7,0)
 
-instLayout.addWidget(ch1InstLable,10,0)
-instLayout.addWidget(ch1InstVal,10,1)
-instLayout.addWidget(ch2InstLable,11,0)
-instLayout.addWidget(ch2InstVal,11,1)
-instLayout.addWidget(ch3InstLable,12,0)
-instLayout.addWidget(ch3InstVal,12,1)
 
+instLayout.addWidget(frontMotorInstLable,0,1)
+instLayout.addWidget(rightMotorInstLable,1,1)
+instLayout.addWidget(rearMotorInstLable,2,1)
+instLayout.addWidget(leftMotorInstLable,3,1)
+
+instLayout.addWidget(ch1InstLable,4,1)
+instLayout.addWidget(ch2InstLable,5,1)
+instLayout.addWidget(ch3InstLable,6,1)
+instLayout.addWidget(yawSetPointInstLable,7,1)
 
 #Display widget as a new window
 w.show()
 
 timer = pg.QtCore.QTimer()
+
 
 def update():
     	checkData()
@@ -180,7 +141,7 @@ def update():
         
 
 timer.timeout.connect(update)
-timer.start(15)
+timer.start(100)
 
 def checkForReadings(data):
     y = re.findall(r"y([-+]?\d*\.\d+|\d+)",data)
@@ -188,31 +149,41 @@ def checkForReadings(data):
     r = re.findall(r"r([-+]?\d*\.\d+|\d+)",data)
     ps = re.findall(r"ps([-+]?\d*\.\d+|\d+)",data)
     rs = re.findall(r"rs([-+]?\d*\.\d+|\d+)",data)
+    ys = re.findall(r"ys([-+]?\d*\.\d+|\d+)",data)
     po = re.findall(r"po([-+]?\d*\.\d+|\d+)",data)
     ro = re.findall(r"ro([-+]?\d*\.\d+|\d+)",data)
     m0 = re.findall(r"m0-([-+]?\d*\.\d+|\d+)",data)
     m1 = re.findall(r"m1-([-+]?\d*\.\d+|\d+)",data)
     m2 = re.findall(r"m2-([-+]?\d*\.\d+|\d+)",data)
     m3 = re.findall(r"m3-([-+]?\d*\.\d+|\d+)",data)
-    # rr = re.findall(r"rr([-+]?\d*\.\d+|\d+)",data)
-    # rp = re.findall(r"rp([-+]?\d*\.\d+|\d+)",data)
-    # rt = re.findall(r"rt([-+]?\d*\.\d+|\d+)",data)
+    rr = re.findall(r"rr([-+]?\d*\.\d+|\d+)",data)
+    rp = re.findall(r"rp([-+]?\d*\.\d+|\d+)",data)
+    rt = re.findall(r"rt([-+]?\d*\.\d+|\d+)",data)
     
     if len(p) > 0 and len(ps) > 0:
     	pitchVal.append(float(p[0]))
         pitchSetPoint.append(float(ps[0]))
+        pitchInstLable.setText('Pitch --> '+p[0])
+        pitchSetPointInstLable.setText('Pitch SetPoint --> '+ps[0])
         while len(pitchVal) > 100 :
             pitchVal.pop(0) #remove the first element
             pitchSetPoint.pop(0)
 
     if len(r) > 0 and len(rs) > 0:
+        rollInstLable.setText('Roll --> ' + r[0])
+        rollSetPointInstLable.setText('Roll SetPoint --> '+rs[0])
     	rollVal.append(float(r[0]))
         rollSetPoint.append(float(rs[0]))
         while len(rollVal) > 100 :
             rollVal.pop(0)
             rollSetPoint.pop(0)
 
+
     if len(m0) > 0  and len(m1) > 0 and len(m2) > 0 and len(m3) > 0:
+        frontMotorInstLable.setText('Front --> '+m0[0])
+        rightMotorInstLable.setText('Right --> '+m1[0])
+        rearMotorInstLable.setText('Rear  --> '+m2[0])
+        leftMotorInstLable.setText('Left  --> '+m3[0])
         m0Val.append(float(m0[0]))
         m1Val.append(float(m1[0]))
         m2Val.append(float(m2[0]))
@@ -223,6 +194,16 @@ def checkForReadings(data):
             m2Val.pop(0)
             m3Val.pop(0) 
 
+    if len(rr) > 0 :
+        ch1InstLable.setText('CH1 --> '+rr[0])
+    if len(rp) > 0 :
+        ch2InstLable.setText('CH2 --> '+rp[0])
+    if len(rt) > 0 :
+        ch3InstLable.setText('CH3 --> '+rt[0])
+    if len(po) > 0 :
+        pitchPIDOutInstLable.setText('Pitch PID Out --> '+po[0])
+    if len(ro) > 0 :
+        rollPIDOutInstLable.setText('Roll PID Out --> '+ro[0])
     # print [y,p,r,ps,rs,po,ro,m0,m1,m2,m3]
 
 def checkData():
@@ -233,6 +214,7 @@ def checkData():
         recvString = recvString + arduinoData.read()
         #recvText.setText(recvString)
     checkForReadings(recvString)
+    app.processEvents()
 
 #start qt event loop
 app.processEvents()
